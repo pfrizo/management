54e2495845214c2e0a40add8047a49e4d158f328
pfrizo <pedrofrizo@hotmail.com>
2025-01-20 20:08:03 -0300

    feat(database): create database and models for tables

---
+++ app/Models/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    //
+
+    public function detail(){
+        return $this->hasOne(UserDetail::class);
+    }
+
+    public function department(){
+        return $this->belongsTo(Department::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     echo 'hello';
+});
+
+Route::get('/email', function(){
+    Mail::raw('management test message', function(Message $message){
+        $message->to('test@gmail.com')
+        ->subject('Welcome!')
+        ->from('frz@management.com');
+    });
+
+    echo 'email susccessfully sended!';
 });
